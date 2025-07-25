901456c4c2dd846d915f2525ed3588e4c17b66b5
Lilian Wuo <wuolilian@gmail.com>
2025-07-25 16:30:43 -0300

    adiciona classes Tabuada, ComparaIdade e DefineIdade; ajusta importações

---
+++ src/estruturacontrole/Tabuada.java
@@ -0,0 +1,5 @@
+package estruturacontrole;
+
+public class Tabuada {
+
+}
--- src/fundamentos/ComparaIdade.java
+++ src/fundamentos/ComparaIdade.java
@@ -1,4 +1,5 @@
 package fundamentos;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
